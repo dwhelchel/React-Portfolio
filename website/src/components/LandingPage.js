ba7e70a31d7b75d8c2ad64a3c613b3a3ac8699dc
Dakota Whelchel <dakota@Dakotas-iMac.local>
2019-05-07 16:09:58 -0700

    changed gradient and added on hover css + social buttons

--- website/src/components/LandingPage.js
+++ website/src/components/LandingPage.js
@@ -7,12 +7,20 @@ class LandingPage extends Component {
 			<div className="landing-page">
 				<Grid className="landing-grid">
 					<Cell col={12}>
+						<div className="portrait-container">
+							<div className="portrait"></div>
+						</div>
 						<div className="banner-text">
 							<h1>Front-End Web Developer</h1>
 							<hr />
 							<p>CSS | HTML5 | C# | C++ | PosgresSQL | JavaScript | React | Angular</p>
 							<div className="socials">
-								<a href="https://github.com/dwhelchel" target="_blank"></a>
+								<a href="https://github.com/dwhelchel" rel="noopener noreferrer" target="_blank">
+									<i className="fa fa-github" aria-hidden="true" />
+								</a>
+								<a href="https://www.linkedin.com/in/dakota-whelchel" rel="noopener noreferrer" target="_blank">
+									<i className="fa fa-linkedin" aria-hidden="true" />
+								</a>
 							</div>
 						</div>
 					</Cell>
