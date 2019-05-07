7240c22dcd919f24ff006bef76e65110e97f72d7
Dakota Whelchel <dakota@Dakotas-iMac.local>
2019-05-07 00:15:47 -0700

    added some landing page stuff

--- website/src/App.js
+++ website/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import { Layout, Header, Navigation, Drawer, Content, Textfield } from 'react-mdl';
 import { Link } from 'react-router-dom';
 import Main from './components/Main';
 import './App.css';
@@ -7,32 +7,30 @@ import './App.css';
 class App extends Component {
   render() {
       return (
-          <div>
-            <div style={{height: '300px', position: 'relative'}}>
-                <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
-                    <Header transparent title="Title" style={{color: 'white'}}>
-                        <Navigation>
-                            <Link to="/resume">Resume</Link>
-                            <Link to="/aboutme">About Me</Link>
-                            <Link to="/projects">Projects</Link>
-                            <Link to="/contact">Contact</Link>
-                        </Navigation>
-                    </Header>
-                    <Drawer title="Title">
-                        <Navigation>
-                            <Link to="/resume">Resume</Link>
-                            <Link to="/aboutme">About Me</Link>
-                            <Link to="/projects">Projects</Link>
-                            <Link to="/contact">Contact</Link>
-                        </Navigation>
-                    </Drawer>
-                    <Content>
-                        <Main />
-                    </Content>
-                </Layout>
-            </div>
-          </div>
-      )
+          <div className="demo-big-content">
+            <Layout>
+                <Header className="header-color" title="Title" scroll>
+                    <Navigation>
+                        <Link to="/resume">Resume</Link>
+                        <Link to="/aboutme">About Me</Link>
+                        <Link to="/projects">Projects</Link>
+                        <Link to="/contact">Contact</Link>
+                    </Navigation>
+                </Header>
+                <Drawer title="Title">
+                    <Navigation>
+                        <Link to="/resume">Resume</Link>
+                        <Link to="/aboutme">About Me</Link>
+                        <Link to="/projects">Projects</Link>
+                        <Link to="/contact">Contact</Link>
+                    </Navigation>
+                </Drawer>
+                <Content>
+                    <Main />
+                </Content>
+            </Layout>
+        </div>
+    );
   }
 }
 
--- website/src/components/LandingPage.js
+++ website/src/components/LandingPage.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class LandingPage extends Component {
 	render() {
 		return (
-			<div><h1>Landing Page</h1></div>
+			<div className="landing-page">
+				<Grid className="landing-grid">
+					<Cell col={12}>
+						<div className="banner-text">
+							<h1>Front-End Web Developer</h1>
+							<hr />
+							<p>CSS | HTML5 | C# | C++ | PosgresSQL | JavaScript | React | Angular</p>
+							<div className="socials">
+								<a href="https://github.com/dwhelchel" target="_blank"></a>
+							</div>
+						</div>
+					</Cell>
+				</Grid>
+			</div>
 		)
 	}
 }
